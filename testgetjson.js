91160d0891cc1851d2c5e859c2750cce34d1be38
radproject <radhouanef@gmail.com>
2014-04-10 16:02:09 +0100

    initial hack

--- testgetjson.js
+++ testgetjson.js
@@ -1,15 +1,32 @@
-$.ajax({
+/*$.ajax({
     ajaxUrl:'testjson.json',
     type: 'json',
     ajax: true,
     success: function(data){
         alert('a sphincter clicks ok');
             alert('sphincter...');
-    $('#my-ajax-table').dynatable({
+   $('#my-ajax-table').dynatable({
     dataset: {
     records: data
       }
     });
   }
 });
-
+*/
+$(document).ready(function() {
+    
+    $.ajax({
+        url: "./Bangla2001ParlResults.json",
+         }).done(function(data) {
+           // var  myRecords = JSON.parse(data);
+            console.log(data);
+            $('#my-table').dynatable({
+      dataset: {
+        records: data
+     }
+    }); 
+        });
+    
+    
+   
+});
