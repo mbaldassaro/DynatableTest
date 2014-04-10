2bb099bdc4115a73bad2b4df256e6594f826d9aa
radproject <radhouanef@gmail.com>
2014-04-10 16:34:35 +0100

    switch to datatables

--- testgetjson.js
+++ testgetjson.js
@@ -16,17 +16,30 @@
 $(document).ready(function() {
     
     $.ajax({
-        url: "./Bangla2001ParlResults.json",
-         }).done(function(data) {
-           // var  myRecords = JSON.parse(data);
-            console.log(data);
-            $('#my-table').dynatable({
-      dataset: {
-        records: data
-     }
-    }); 
-        });
-    
-    
+      url: "Bangla2001ParlResults.json"
+    }).done(function(data) {
+      var oTable = $('#my-table').dataTable( {
+        
+        "aaData": data,
+        "aoColumns": [
+            { "mData": "year" },
+            { "mData": "constituency Number" },
+            { "mData": "district" },
+            { "mData": "registered Voters" },
+            { "mData": "valid Votes" },
+            { "mData": "voter Turnout Percentage" },
+            { "mData": "four Party Alliance Votes" },
+            { "mData": "four Party Alliance Percentage" },
+            { "mData": "awami League" },
+            { "mData": "awami Leage Percentage" },
+            { "mData": "jatiya Party" },
+            { "mData": "jatiya Party Percentage" },
+            { "mData": "others" },
+            { "mData": "others Percentage" },
+            { "mData": "winner" }
    
-});
+        ]
+    } );
+    });
+    
+} );
